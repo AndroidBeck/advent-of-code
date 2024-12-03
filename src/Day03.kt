5fd806b0423380a112c1b4509f771183c2b78c14
AEvd <alexandr8@gmail.com>
2024-12-03 12:55:11 +0300

    Day 03 task 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -12,13 +12,34 @@ fun parseInputAndCalcMultiplicationsSum(input: String): Int {
     return sum
 }
 
+fun calcMultSumWithStartStop(input: String) = parseInputAndCalcMultiplicationsSum(extractDoSubsequence(input))
+
+private fun extractDoSubsequence(input: String): String {
+    val startStr = "do()"
+    val stopStr = "don't()"
+    val output = StringBuilder()
+    var taking = true
+    var i = 0
+    while (i < input.length) {
+        if (input.startsWith(stopStr, i)) {
+            taking = false
+            i += stopStr.length - 1
+        } else if (input.startsWith(startStr, i)) {
+            taking = true
+            i += startStr.length - 1
+        } else if (taking) output.append(input[i])
+        i++
+    }
+    return output.toString()
+}
+
 fun main() {
     check(parseInputAndCalcMultiplicationsSum("Some text mul(1,2) other text mul(3,5) and mul(3,4)\n") == 29)
     check(parseInputAndCalcMultiplicationsSum("Some text mul(1, 2) other text mul(3,5) and mul(3,4)\n") == 27)
 
-//    val testInput = readInput("Day03_test")
-//    check(parseInputAndCalcMultiplicationsSum(testInput) == 3)
-
     val input = readText("Day03")
     parseInputAndCalcMultiplicationsSum(input).println()
+
+    check(calcMultSumWithStartStop("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))") == 48)
+    calcMultSumWithStartStop(input).println()
 }
