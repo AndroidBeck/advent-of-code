43516fefd5a6daa7ee1190aea8e9bc75903af1f0
AEvd <alexandr8@gmail.com>
2024-12-04 11:33:54 +0300

    Day 04 task 2

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,86 +1,100 @@
+fun left(i: Int, j: Int, input: List<String>, word: String): Boolean {
+    if (j < word.length - 1) return false
+    for (k in word.indices) {
+        if (input[i][j - k] != word[k]) return false
+    }
+    return true
+}
+
+fun up(i: Int, j: Int, input: List<String>, word: String): Boolean {
+    if (i < word.length - 1) return false
+    for (k in word.indices) {
+        if (input[i - k][j] != word[k]) return false
+    }
+    return true
+}
+
+fun right(i: Int, j: Int, input: List<String>, word: String): Boolean {
+    if (j > input.size - word.length) return false
+    for (k in word.indices) {
+        if (input[i][j + k] != word[k]) return false
+    }
+    return true
+}
+
+fun down(i: Int, j: Int, input: List<String>, word: String): Boolean {
+    if (i > input.size - word.length) return false
+    for (k in word.indices) {
+        if (input[i + k][j] != word[k]) return false
+    }
+    return true
+}
+
+fun leftUp(i: Int, j: Int, input: List<String>, word: String): Boolean {
+    if (j < word.length - 1) return false
+    if (i < word.length - 1) return false
+    for (k in word.indices) {
+        if (input[i - k][j - k] != word[k]) return false
+    }
+    return true
+}
+
+fun rightUp(i: Int, j: Int, input: List<String>, word: String): Boolean {
+    if (j > input.size - word.length) return false
+    if (i < word.length - 1) return false
+    for (k in word.indices) {
+        if (input[i - k][j + k] != word[k]) return false
+    }
+    return true
+}
+
+fun leftDown(i: Int, j: Int, input: List<String>, word: String): Boolean {
+    if (j < word.length - 1) return false
+    if (i > input.size - word.length) return false
+    for (k in word.indices) {
+        if (input[i + k][j - k] != word[k]) return false
+    }
+    return true
+}
+
+fun rightDown(i: Int, j: Int, input: List<String>, word: String): Boolean {
+    if (j > input.size - word.length) return false
+    if (i > input.size - word.length) return false
+    for (k in word.indices) {
+        if (input[i + k][j + k] != word[k]) return false
+    }
+    return true
+}
+
 fun searchForXMAS(input: List<String>): Int {
-    val n = input.size  // 140
-    val xmas = "XMAS"
+    val word = "XMAS"
     var xmasNum = 0
-
-    fun left(i: Int, j: Int): Boolean {
-        if (j < xmas.length - 1) return false
-        for (k in xmas.indices) {
-            if (input[i][j - k] != xmas[k]) return false
-        }
-        return true
-    }
-
-    fun up(i: Int, j: Int): Boolean {
-        if (i < xmas.length - 1) return false
-        for (k in xmas.indices) {
-            if (input[i - k][j] != xmas[k]) return false
-        }
-        return true
-    }
-
-    fun right(i: Int, j: Int): Boolean {
-        if (j > n - xmas.length) return false
-        for (k in xmas.indices) {
-            if (input[i][j + k] != xmas[k]) return false
-        }
-        return true
-    }
-
-    fun down(i: Int, j: Int): Boolean {
-        if (i > n - xmas.length) return false
-        for (k in xmas.indices) {
-            if (input[i + k][j] != xmas[k]) return false
-        }
-        return true
-    }
-
-    fun leftUp(i: Int, j: Int): Boolean {
-        if (j < xmas.length - 1) return false
-        if (i < xmas.length - 1) return false
-        for (k in xmas.indices) {
-            if (input[i - k][j - k] != xmas[k]) return false
-        }
-        return true
-    }
-
-    fun rightUp(i: Int, j: Int): Boolean {
-        if (j > n - xmas.length) return false
-        if (i < xmas.length - 1) return false
-        for (k in xmas.indices) {
-            if (input[i - k][j + k] != xmas[k]) return false
-        }
-        return true
-    }
-
-    fun leftDown(i: Int, j: Int): Boolean {
-        if (j < xmas.length - 1) return false
-        if (i > n - xmas.length) return false
-        for (k in xmas.indices) {
-            if (input[i + k][j - k] != xmas[k]) return false
-        }
-        return true
-    }
-
-    fun rightDown(i: Int, j: Int): Boolean {
-        if (j > n - xmas.length) return false
-        if (i > n - xmas.length) return false
-        for (k in xmas.indices) {
-            if (input[i + k][j + k] != xmas[k]) return false
-        }
-        return true
-    }
-    
     input.forEachIndexed { i, line ->
         line.forEachIndexed { j, c ->
-            if (left(i, j)) xmasNum++
-            if (up(i, j)) xmasNum++
-            if (right(i, j)) xmasNum++
-            if (down(i, j)) xmasNum++
-            if (leftUp(i, j)) xmasNum++
-            if (rightUp(i, j)) xmasNum++
-            if (leftDown(i, j)) xmasNum++
-            if (rightDown(i, j)) xmasNum++
+            if (left(i, j, input, word)) xmasNum++
+            if (up(i, j, input, word)) xmasNum++
+            if (right(i, j, input, word)) xmasNum++
+            if (down(i, j, input, word)) xmasNum++
+            if (leftUp(i, j, input, word)) xmasNum++
+            if (rightUp(i, j, input, word)) xmasNum++
+            if (leftDown(i, j, input, word)) xmasNum++
+            if (rightDown(i, j, input, word)) xmasNum++
+        }
+    }
+    return xmasNum
+}
+
+fun searchForXLikeMAS(input: List<String>): Int {
+    var xmasNum = 0
+    input.forEachIndexed { i, line ->
+        line.forEachIndexed { j, c ->
+            if (c != 'A') return@forEachIndexed
+            var acc = 0
+            if (leftUp(i, j, input, "AM") && rightDown(i, j, input, "AS")) acc++
+            else if (leftUp(i, j, input, "AS") && rightDown(i, j, input, "AM")) acc++
+            if (rightUp(i, j, input, "AM") && leftDown(i, j, input, "AS")) acc++
+            else if (rightUp(i, j, input, "AS") && leftDown(i, j, input, "AM")) acc++
+            if (acc == 2) xmasNum++
         }
     }
     return xmasNum
@@ -92,4 +106,8 @@ fun main() {
 
     val input = readInput("Day04")
     searchForXMAS(input).println()
+
+    val testInput2 = readInput("Day04_test")
+    check(searchForXLikeMAS(testInput2) == 9)
+    searchForXLikeMAS(input).println()
 }
