e0cf3c72b90774ddb69c38e8af650849ec2c5455
AEvd <alexandr8@gmail.com>
2024-12-06 14:42:07 +0300

    Day 06 task 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -7,10 +7,6 @@ enum class Direction {
         WEST -> NORTH
     }
 }
-fun Direction.isNORTH() = this == Direction.NORTH
-fun Direction.isEAST() = this == Direction.EAST
-fun Direction.isSOUTH() = this == Direction.SOUTH
-fun Direction.isWEST() = this == Direction.WEST
 
 private fun findSymbol(tiles: List<CharArray>, symbol: Char): Pair<Int, Int> {
     tiles.forEachIndexed { y, line ->
@@ -34,9 +30,7 @@ fun calcVisitedTiles(tiles: List<CharArray>): Int {
     val n = tiles.size
     var (x ,y) = findSymbol(tiles, '^')
     var direction = Direction.NORTH
-    tiles[y][x] = 'X'
     var visitedTiles = 1
-
     // Simulate
     while(true) {
         val (nextX, nextY) = moveInDirection(direction, x, y)
@@ -53,8 +47,57 @@ fun calcVisitedTiles(tiles: List<CharArray>): Int {
             }
         }
     }
-
     return visitedTiles
+}
+
+// Part 2
+typealias Coordinate = Pair<Int, Int>
+
+private fun List<CharArray>.findAllSymbols(symbol: Char): List<Coordinate> {
+    val places = mutableListOf<Coordinate>()
+    this.forEachIndexed { y, line ->
+        line.forEachIndexed { x, c -> if (c == symbol) places.add(Coordinate(x, y)) }
+    }
+    return places
+}
+
+private fun isCycled(startXY: Coordinate, obstacles: Set<Coordinate>, n: Int, startDir: Direction): Boolean {
+    var (x, y) = startXY
+    var direction = startDir
+    val visitedObst = mutableMapOf<Coordinate, MutableSet<Direction>>()
+    // Simulate
+    while(true) {
+        val next = moveInDirection(direction, x, y)
+        val (nextX, nextY) = next
+        if (nextX < 0 || nextX > n - 1 || nextY < 0 || nextY > n - 1) {
+            break
+        } else if (next in obstacles) {
+            val added = visitedObst.computeIfAbsent(next) { mutableSetOf() }.add(direction)
+            if (!added) return true
+            direction = direction.turnRight()
+        } else {
+            x = nextX
+            y = nextY
+        }
+    }
+    return false
+}
+
+fun calcPlacesForObstacle(tiles: List<CharArray>): Int {
+    val n = tiles.size
+    var placesForObst = 0
+    val startXY = findSymbol(tiles, '^')
+    val direction = Direction.NORTH
+
+    calcVisitedTiles(tiles)
+    val possiblePlaces = tiles.findAllSymbols('X')
+    val obstacles = tiles.findAllSymbols('#').toMutableSet()
+    possiblePlaces.forEach { place ->
+        obstacles.add(place)
+        if (isCycled(startXY, obstacles, n, direction)) placesForObst++
+        obstacles.remove(place)
+    }
+    return placesForObst
 }
 
 fun main() {
@@ -63,4 +106,10 @@ fun main() {
 
     val input = readInput("Day06").map { it.toCharArray() } // 130 x 130
     calcVisitedTiles(input).println()
+
+    // Part 2 (inputs are changed here)
+    val testInput2 = readInput("Day06_test").map { it.toCharArray() } // 10 x 10
+    check(calcPlacesForObstacle(testInput2) == 6)
+    val input2 = readInput("Day06").map { it.toCharArray() } // 130 x 130
+    calcPlacesForObstacle(input2).println()
 }
