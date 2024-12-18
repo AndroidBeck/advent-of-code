dfcd8b17fe59caad03107827e7cf3ae1a61c2950
AEvd <alexandr8@gmail.com>
2024-12-18 17:21:34 +0300

    Day 18 task 1

--- src/Day18.kt
+++ src/Day18.kt
@@ -1,29 +1,33 @@
-private const val EMPTY = '.'
-private const val START = 'S'
-private const val END = 'E'
-private val passable = setOf(EMPTY, END)
-
-fun minStepsNumber(input: List<String>, xMax: Int = 70, yMax: Int = 70, debug: Boolean = false): Int {
+fun minStepsNumber(input: List<String>, fallen: Int, xMax: Int = 70, yMax: Int = 70, p2: Boolean = false, debug: Boolean = false): Int {
     val corruptBlocks = input.map { it.split(",").map { s -> s.toInt() } }.map { l -> Coordinate(l[0], l[1]) }
     val corruptMap = mutableMapOf<Coordinate, Int>() // coord -> time of corruption
-    corruptBlocks.forEachIndexed { i, c -> corruptMap[c] = i }
-
     val isSafeMatrix = Array(yMax + 1) { BooleanArray(xMax + 1)  { true } }
-    corruptBlocks.forEachIndexed { i, c -> if (i < c.sumXY()) isSafeMatrix[c.y()][c.x()] = false }
-
     val steps = Array(yMax + 1) { IntArray(xMax + 1) { Int.MAX_VALUE } }
+
     fun Coordinate.isSafe() = x() in 0..xMax && y() in 0..yMax && isSafeMatrix.get(this)
 
     val start = Coordinate(0, 0)
     val finish = Coordinate(xMax, yMax)
     val path = mutableListOf<Coordinate>()
-    var minSteps = Int.MAX_VALUE
 
     fun dfs(c: Coordinate, step: Int) {
         if (step < steps.get(c)) steps.set(c, step) else return
-        if (c == finish) minSteps = step
-        c.getNeighbours().forEach { neighbourC -> if (neighbourC.isSafe()) dfs(neighbourC,  step + 1) }
-        return
+        if (c != finish) c.getNeighbours().forEach { neighbourC -> if (neighbourC.isSafe()) dfs(neighbourC,  step + 1) } else return
+    }
+
+    if (!p2) {
+        for (i in 0..< fallen) {
+            val c = corruptBlocks[i]
+            corruptMap[c] = i
+            isSafeMatrix.set(c, false)
+        }
+        dfs(start, 0)
+        return steps.get(finish)
+    }
+
+    corruptBlocks.forEachIndexed { i, c ->
+        corruptMap[c] = i
+        if (i < c.sumXY()) isSafeMatrix.set(c, false)
     }
 
     fun dfsBack(c: Coordinate) {
@@ -31,7 +35,7 @@ fun minStepsNumber(input: List<String>, xMax: Int = 70, yMax: Int = 70, debug: B
         if (c == start) return
         val possibleC = c.getNeighbours().filter { it.isSafe() }
         val minValue = possibleC.minOf { it.getValueIn(steps) }
-        val next = possibleC.first { it.getValueIn(steps) == minValue}
+        val next = possibleC.first { it.getValueIn(steps) == minValue }
         dfsBack(next)
     }
 
@@ -98,9 +102,6 @@ private fun printDebugInfo(isSafeMatrix: Array<BooleanArray>, corruptMap: Map<Co
 fun main() {
     val testInput = readInput("Day18_test")
     val input = readInput("Day18")
-
-    minStepsNumber(testInput, xMax = 6, yMax = 6, debug = true).println()
-
-//    check(minStepsNumber(testInput, xMax = 6, yMax = 6) == 22)
-//    minScore(minStepsNumber).println()
+    check(minStepsNumber(testInput, xMax = 6, yMax = 6, fallen = 12) == 22)
+    minStepsNumber(input, fallen = 1024).println()
 }
